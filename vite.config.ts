4842ef8b482f879ca48bdd60e37aa035edc3939f
Gabriel Glatz <gabriel.glatz@malwee.com.br>
2025-03-07 13:45:29 -0300

    chore: configure Vite to exclude the 'pages' folder from the 'dist' directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path';
-
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src') // Isso configura o alias para apontar para a pasta src
-    }
-  }
-})
-
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: [path.resolve(__dirname, "src/pages")],
+    },
+  },
+});
