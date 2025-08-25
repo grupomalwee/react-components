c700670c155fe5c7d763f9ab9be0bc66a4f91ef4
Gabriel Glatz <gabriel.glatz@malwee.com.br>
2025-08-25 16:31:22 -0300

    chore: update package dependencies and remove unused test files

--- tests/home.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Home', async ({ page }) => {
-  await page.goto('http://localhost:5173');
-
-  await expect(page.getByRole('heading', { name: /Bem-vindo ao/i })).toBeVisible();
-
-});
