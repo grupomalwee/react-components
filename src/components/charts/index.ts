e2cdc8b8936d9146b68ea11f5efd81cd3bc3d8fc
glatz-gabriel <gabriel.glatz@malwee.com.br>
2025-10-08 13:39:56 -0300

    feat: atualizar versão para 1.5.8 e adicionar hooks de gráficos

--- src/components/charts/index.ts
+++ src/components/charts/index.ts
@@ -1,2 +1,7 @@
+export { default as Chart } from "./Chart";
+export { default as BarChart } from "./BarChart";
+export { default as LineChart } from "./LineChart";
+export { default as PieChart } from "./PieChart";
 export * from "./components";
+export * from "./hooks";
 export * from "./utils";
