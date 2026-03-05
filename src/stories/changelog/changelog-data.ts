0c4523e2ffc3ec00a1425102c129c31d02cd5f8f
glatz-gabriel <gabriel.glatz@malwee.com.br>
2026-03-05 15:31:23 -0300

    1.10.14

--- src/stories/changelog/changelog-data.ts
+++ src/stories/changelog/changelog-data.ts
@@ -11,6 +11,12 @@ export type ChangeItem = {
 
 export const changelogData: ChangeItem[] = [
   {
+    version: "1.10.14",
+    date: "2026-03-05",
+    shortTitle: "Event Calendar View",
+    fixed: ["Event Calendar View: arrumado visual."],
+  },
+  {
     version: "1.10.13",
     date: "2026-03-05",
     shortTitle: "Dialog",
